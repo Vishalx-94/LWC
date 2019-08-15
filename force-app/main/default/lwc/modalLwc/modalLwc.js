9da3d28b1bb21fc9dda133f955c5d21da6090542
vishalx-94 <vishalx.suryavanshi@popcornapps.com>
2019-08-15 22:11:04 +0530

    Trailhead example

--- force-app/main/default/lwc/modalLwc/modalLwc.js
+++ force-app/main/default/lwc/modalLwc/modalLwc.js
@@ -1,5 +1,9 @@
-import { LightningElement} from 'lwc';
- 
-export default class ModalLwc extends LightningElement {
-     
+import { LightningElement } from 'lwc';
+export default class App extends LightningElement {
+   name = 'Electra X4';
+   description = 'A sweet bike built for comfort.';
+   category = 'Mountain';
+   material = 'Steel';
+   price = '$2,700';
+   pictureUrl = 'https://s3-us-west-1.amazonaws.com/sfdc-demo/ebikes/electrax4.jpg';
 }
